834b190b22825a4bccd223abba5997fad83479e2
Jacob Hartman <junyunharold1@gmail.com>
2022-04-05 23:22:20 -0400

    More test fixes

--- test/test.sh
+++ test/test.sh
@@ -20,9 +20,9 @@ docker stop ${NAME} 2>/dev/null
 docker run -d --rm --cap-add=NET_ADMIN --cap-add=NET_RAW --name ${NAME} -it ports4u
 
 GATEWAY=$(docker inspect ${NAME} | grep "Gateway\":" | head -n 1 | sed 's_^[ \t]*"Gateway": "\(.*\)".*_\1_')
-IP_ADDR=$(docker inspect ${NAME} | grep "IPAddress\":" | head -n 1 | sed 's_^[ \t]*"IPAddress": "\(.*\)".*_\1_')
+DIRECT_IP=$(docker inspect ${NAME} | grep "IPAddress\":" | head -n 1 | sed 's_^[ \t]*"IPAddress": "\(.*\)".*_\1_')
 
-echo "hello how are you" | nc -q 2 ${IP_ADDR} 8080
+echo "hello how are you" | nc -q 2 ${DIRECT_IP} 8080
 
 echo ""
 echo "\nChecking basic ports"
@@ -38,7 +38,7 @@ echo "\nChecking default HTTP"
 
 LOG_NAME=${GATEWAY}-80.log
 
-curl -s http://${IP_ADDR} >/dev/null
+curl -s http://${DIRECT_IP} >/dev/null
 
 docker cp ${NAME}:/opt/ports4u/logs/${LOG_NAME} /tmp/${LOG_NAME}
 
@@ -51,7 +51,7 @@ echo "\nChecking default HTTPS"
 
 LOG_NAME=${GATEWAY}-443.log
 
-curl -s -k https://${IP_ADDR} >/dev/null
+curl -s -k https://${DIRECT_IP} >/dev/null
 
 docker cp ${NAME}:/opt/ports4u/logs/${LOG_NAME} /tmp/${LOG_NAME}
 
@@ -66,7 +66,7 @@ echo "Checking offport HTTP"
 
 LOG_NAME=${GATEWAY}-9999.log
 
-curl -s http://${IP_ADDR}:9999 >/dev/null
+curl -s http://${DIRECT_IP}:9999 >/dev/null
 
 docker cp ${NAME}:/opt/ports4u/logs/${LOG_NAME} /tmp/${LOG_NAME}
 
@@ -81,7 +81,7 @@ echo "Checking offport HTTPS"
 
 LOG_NAME=${GATEWAY}-3000.log
 
-curl -s -k https://${IP_ADDR}:3000 >/dev/null
+curl -s -k https://${DIRECT_IP}:3000 >/dev/null
 
 docker cp ${NAME}:/opt/ports4u/logs/${LOG_NAME} /tmp/${LOG_NAME}
 
@@ -96,11 +96,11 @@ echo "Checking DNS"
 
 LOG_NAME=dns-test
 
-dig @${IP_ADDR} another.com 
-dig @${IP_ADDR} test.com > /tmp/${LOG_NAME}
+dig @${DIRECT_IP} another.com 
+dig @${DIRECT_IP} test.com > /tmp/${LOG_NAME}
 sed -i 's_\t_ _g' /tmp/${LOG_NAME}
 verify_data ${LOG_NAME} "test.com."
-verify_data ${LOG_NAME} "IN A ${IP_ADDR}"
+verify_data ${LOG_NAME} "IN A ${DIRECT_IP}"
 verify_data ${LOG_NAME} ";; ANSWER SECTION:"
 rm /tmp/${LOG_NAME}
 
@@ -114,12 +114,12 @@ rm /tmp/${LOG_NAME}
 echo ""
 echo "Checking redirection"
 
-sudo ip route add 192.168.55.0/24 via ${IP_ADDR}
-TEST_IP="192.168.55.4"
+sudo ip route add 192.168.55.0/24 via ${DIRECT_IP}
+EXTERNAL_IP="192.168.55.4"
 
 LOG_NAME=${GATEWAY}-4545.log
 
-curl -s http://${TEST_IP}:4545 >/dev/null
+curl -s http://${EXTERNAL_IP}:4545 >/dev/null
 
 docker cp ${NAME}:/opt/ports4u/logs/${LOG_NAME} /tmp/${LOG_NAME}
 
@@ -134,7 +134,7 @@ echo "Checking empty connection (no data)"
 
 LOG_NAME=${GATEWAY}-7070.log
 
-nc -z ${TEST_IP} 7070 
+nc -z ${EXTERNAL_IP} 7070 
 
 docker cp ${NAME}:/opt/ports4u/logs/${LOG_NAME} /tmp/${LOG_NAME}
 verify_data ${LOG_NAME} "<<<<<<<< ${GATEWAY}"
@@ -144,7 +144,8 @@ rm /tmp/${LOG_NAME}
 echo ""
 echo "Testing basic UDP"
 
-echo "hi" | nc -w 1 -u ${TEST_IP} 1111 
+echo "hi" | nc -w 1 -u ${DIRECT_IP} 1111 
+echo "hi" | nc -w 1 -u ${EXTERNAL_IP} 1112
 
 echo ""
 echo "Testing conn_list"
@@ -152,13 +153,14 @@ echo "Testing conn_list"
 LOG_NAME=conn_list.txt
 
 docker cp ${NAME}:/opt/ports4u/logs/${LOG_NAME} /tmp/${LOG_NAME}
-verify_data ${LOG_NAME} "tcp:${TEST_IP}:80"
-verify_data ${LOG_NAME} "tcp:${TEST_IP}:8080"
-verify_data ${LOG_NAME} "tcp:${TEST_IP}:9999"
-verify_data ${LOG_NAME} "tcp:${TEST_IP}:3000"
-verify_data ${LOG_NAME} "tcp:${TEST_IP}:443"
-verify_data ${LOG_NAME} "tcp:${TEST_IP}:4545"
-verify_data ${LOG_NAME} "tcp:${TEST_IP}:7070"
-verify_data ${LOG_NAME} "udp:${TEST_IP}:1111"
+verify_data ${LOG_NAME} "tcp:${DIRECT_IP}:80"
+verify_data ${LOG_NAME} "tcp:${DIRECT_IP}:8080"
+verify_data ${LOG_NAME} "tcp:${DIRECT_IP}:9999"
+verify_data ${LOG_NAME} "tcp:${DIRECT_IP}:3000"
+verify_data ${LOG_NAME} "tcp:${DIRECT_IP}:443"
+verify_data ${LOG_NAME} "tcp:${EXTERNAL_IP}:4545"
+verify_data ${LOG_NAME} "tcp:${EXTERNAL_IP}:7070"
+verify_data ${LOG_NAME} "udp:${DIRECT_IP}:1111"
+verify_data ${LOG_NAME} "udp:${DIRECT_IP}:1112"
 
 rm /tmp/${LOG_NAME}
